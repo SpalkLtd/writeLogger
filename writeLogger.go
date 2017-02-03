bcbd09020b77c1a4e2f167bec84e3d6f273c3716
Dion Bramley <dion.bramley@gmail.com>
2017-02-03 13:44:21 +1300

    Working and tests are passing with 100% coverage

---
+++ writeLogger.go
@@ -0,0 +1,66 @@
+package writeLogger
+
+import (
+	"bytes"
+	"io"
+)
+
+const defaultSize uint = 1024
+
+var newSize uint = defaultSize
+
+type WriteLogger struct {
+	buffer []byte
+	out    io.Writer
+}
+
+func NewWriter(target io.Writer) WriteLogger {
+	wl := WriteLogger{
+		out:    target,
+		buffer: make([]byte, newSize, newSize),
+	}
+	return wl
+}
+
+func NewWriterWithSize(target io.Writer, size uint) WriteLogger {
+	wl := WriteLogger{
+		out:    target,
+		buffer: make([]byte, size, size),
+	}
+	return wl
+}
+
+func SetBufferSize(size uint) {
+	newSize = size
+}
+
+func (wl *WriteLogger) Write(p []byte) (n int, err error) {
+	outn, outerr := wl.out.Write(p)
+	bsize := len(wl.buffer)
+	isize := len(p)
+
+	// fmt.Printf("buffer length and cap: %v %v\n", len(wl.buffer), cap(wl.buffer))
+
+	// fmt.Println(bsize)
+	// fmt.Println(isize)
+	// fmt.Println(bsize - isize)
+
+	if isize > bsize {
+		wl.buffer = p[isize-bsize:]
+		// fmt.Printf("%v\n", string(wl.buffer))
+
+	} else {
+		//bsize >= isize
+		wl.buffer = append(wl.buffer[isize:], p...)
+
+	}
+	return outn, outerr
+}
+
+func (wl WriteLogger) Read(n int) []byte {
+	return wl.buffer[len(wl.buffer)-n:]
+}
+
+func (wl WriteLogger) ReadBuffer() *bytes.Buffer {
+	return bytes.NewBuffer(wl.buffer)
+}
---
+++ writeLogger_test.go
@@ -0,0 +1,111 @@
+package writeLogger
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+var toCopy string = "Hello World! The quick brown fox jumps over the lazy dog."
+var tenBytes string = " lazy dog."
+
+func TestCopyAndLog(t *testing.T) {
+	outBuffer := bytes.NewBuffer(nil)
+	writer := NewWriter(outBuffer)
+	writer.Write([]byte(toCopy))
+	outbytes, _ := ioutil.ReadAll(outBuffer)
+
+	out := string(outbytes)
+	if out != toCopy {
+		t.Error(
+			"For", toCopy,
+			"got", out,
+		)
+	}
+
+	end := string(writer.Read(10))
+	if end != tenBytes {
+		t.Error(
+			"For", toCopy, 10,
+			"expected", tenBytes,
+			"got", end,
+		)
+	}
+}
+
+func TestRepeatedWrite(t *testing.T) {
+	outBuffer := bytes.NewBuffer(nil)
+	writer := NewWriter(outBuffer)
+
+	for _, v := range toCopy {
+		writer.Write([]byte{byte(v)})
+	}
+
+	outBuffer = writer.ReadBuffer()
+
+	outbytes, _ := ioutil.ReadAll(outBuffer)
+	out := strings.Trim(string(outbytes), string(rune(0)))
+	if out != toCopy {
+		t.Error(
+			"For", toCopy,
+			"got", out,
+		)
+	}
+}
+
+func TestWriteMoreThanBufferSize(t *testing.T) {
+	outBuffer := bytes.NewBuffer(nil)
+	writer := NewWriterWithSize(outBuffer, 10)
+
+	writer.Write([]byte(toCopy))
+
+	outBuffer = writer.ReadBuffer()
+
+	outbytes, _ := ioutil.ReadAll(outBuffer)
+	out := string(outbytes)
+	if out != tenBytes {
+		t.Error(
+			"For", toCopy,
+			"got", out,
+		)
+	}
+
+	SetBufferSize(10)
+
+	outBuffer = bytes.NewBuffer(nil)
+	writer = NewWriter(outBuffer)
+
+	writer.Write([]byte(toCopy))
+
+	outBuffer = writer.ReadBuffer()
+
+	outbytes, _ = ioutil.ReadAll(outBuffer)
+	out = string(outbytes)
+	if out != tenBytes {
+		t.Error(
+			"For", toCopy,
+			"got", out,
+		)
+	}
+}
+
+func TestWriteMoreThanBufferSizeSlowly(t *testing.T) {
+	outBuffer := bytes.NewBuffer(nil)
+	writer := NewWriterWithSize(outBuffer, 10)
+
+	for _, v := range toCopy {
+		writer.Write([]byte{byte(v)})
+	}
+
+	outBuffer = writer.ReadBuffer()
+
+	outbytes, _ := ioutil.ReadAll(outBuffer)
+	out := string(outbytes)
+	if out != tenBytes {
+		t.Error(
+			"For", toCopy,
+			"got", out,
+		)
+	}
+}
